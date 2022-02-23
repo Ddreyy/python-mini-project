94b5644bbc9dcec5b143a0534cfaacaca87d7c2b
Ddreyy <mradedapodada@gmail.com>
2022-02-23 10:13:24 +0100

    check this project out

---
+++ quiz game.py
@@ -0,0 +1,46 @@
+from lib2to3.pgen2.token import GREATER
+
+
+print("Welcome to my computer quiz!")
+
+playing = input("Do you want to play? ") 
+
+if playing.lower() != "yes":
+    quit()
+    
+player_name = input("Enter your name ")
+
+print("okay! let's play :)")
+score = 0
+
+answer = input("what does CPU stand for? ")
+if answer.lower() == "central processing unit":
+    print('Correct!')
+    score +=1
+else:
+    print("Incorrect!")
+
+answer = input("What does GPU stand for? ")
+if answer.lower() == "graphical processing unit":
+    print('Correct!')
+    score +=1
+else:
+    print("Incorrect!")
+
+
+answer = input("What does RAM stanf for? ")
+if answer.lower() == "random access memory":
+    print('Correct!')
+    score +=1
+else:
+    print("Incorrect!")
+
+answer = input("what does PSU stand for? ")
+if answer.lower() == "power supply":
+    print('Correct!')
+    score +=1
+else:
+    print("Incorrect!")
+
+print("You got " + str(score) + "question correct!")
+print("You got " + str((score / 4) * 100) + "%")    
